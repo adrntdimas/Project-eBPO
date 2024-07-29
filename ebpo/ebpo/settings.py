0ffac0303633824c9578785beb66ab731fdf0c11
Dimas Adrianto <dimas.adrianto@pharos.co.id>
2024-07-29 16:27:39 +0700

    tes

--- ebpo/ebpo/settings.py
+++ ebpo/ebpo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ebpo',
     'rest_framework',
+    'login_app',
 ]
 
 MIDDLEWARE = [
--- ebpo/urls.py
+++ ebpo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),
+    path('api/'),
+    path('auth/', include('login_app.urls')),
 ]
